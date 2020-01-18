4ef807890dd219849f36c956affc6bc8325b842c
mab2400 <55205533+mab2400@users.noreply.github.com>
2020-01-18 11:47:14 -0500

    Update FSMModule.py

--- FSMProject/FSMModule.py
+++ FSMProject/FSMModule.py
@@ -1,1 +1,13 @@
+  # FSM class
+  class FSM:
 
+      def __init__(self):
+          self.start = null
+          self.accepting = []
+          self.intermediary = []
+          self.inputStr = ''
+
+     def addTransition(self, n1, n2, 'c'):
+         # if current state is at node 1  and
+         # the next character read is 'c'
+         # then move to node 2
